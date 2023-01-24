ccfdeaa771de9816f63fdc8661654906f8ca2634
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2023-01-24 18:35:33 +0530

    Update dynamo-db-lock.tf

--- dynamo-db-lock.tf
+++ dynamo-db-lock.tf
@@ -1,7 +1,11 @@
+
 resource "aws_dynamodb_table" "tf_state_lock" {
-  name           = var.dynamodb_table
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
+  hash_key = "LockID"
+  name     = var.dynamodb_table
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
 }
 
