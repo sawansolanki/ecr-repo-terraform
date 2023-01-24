dc20fc68a682b3db10781535617ebd4168bde40c
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2023-01-24 18:00:36 +0530

    Update dynamo-db-lock.tf

--- dynamo-db-lock.tf
+++ dynamo-db-lock.tf
@@ -4,3 +4,11 @@ resource "aws_dynamodb_table" "tf_state_lock" {
   write_capacity = 1
   hash_key       = "LockID"
 }
+
+terraform {
+  backend "s3" {
+    bucket = var.bucket_name
+    key    = "state-file"
+    region = "us-east-1"
+  }
+}
