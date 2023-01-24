5bca85c75a781e65d4e8c90e44478cbb30e595ca
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2023-01-24 17:17:58 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,3 +2,13 @@ variable "repo_name" {
   type = string
   default = "ecr-repo-sa1"
 }
+
+variable "bucket_name" {
+  type = string
+  default = "remote-sts-s3-sa1"
+}
+
+variable "dynamodb_table" {
+  type = string
+  default = "state-lock-db-table-sa1"
+}
